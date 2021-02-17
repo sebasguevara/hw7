f20c1c32aca52c15f5abd78d0f1e18cea2221ceb
Sebastian Guevara <sebastian.guevara@kellogg.northwestern.edu>
2021-02-16 22:17:45 -0600

    checkpoint

--- movies.js
+++ movies.js
@@ -1,33 +1,85 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
-  let db = firebase.firestore()
-  let apiKey = 'your TMDB API key'
-  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
-  let json = await response.json()
-  let movies = json.results
-  console.log(movies)
+let db = firebase.firestore()
+
+firebase.auth().onAuthStateChanged(async function(user)  {
   
-  for (let i=0; i<movies.length; i++) {
-    let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
-    let watchedMovie = docRef.data()
-    let opacityClass = ''
-    if (watchedMovie) {
-      opacityClass = 'opacity-20'
-    }
-
-    document.querySelector('.movies').insertAdjacentHTML('beforeend', `
-      <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
-        <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+  if (user)
+  {
+    
+    // Ensure the signed-in user is in the users collection
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
+    
+    console.log(`signed in as ${user.displayName}`)
+    
+    // Sign-out button
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <div class="sm: flex">
+        <div class="w-1/2 text-left">Signed in as <strong>${user.displayName}</strong></div>
+        <button class="w-1/2 text-pink-500 underline sign-out text-right">Sign Out</button>
       </div>
-    `)
+    `
 
-    document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
-      event.preventDefault()
-      let movieElement = document.querySelector(`.movie-${movie.id}`)
-      movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
-    }) 
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'movies.html'
+    })
+
+
+    let apiKey = 'df22f2b8c840ba54cfecafa78c93269d'
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+    let json = await response.json()
+    let movies = json.results
+    console.log(movies)
+    
+    for (let i=0; i<movies.length; i++) {
+      let movie = movies[i]
+      let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+      let watchedMovie = docRef.data()
+      let opacityClass = ''
+      if (watchedMovie) {
+        opacityClass = 'opacity-20'
+      }
+
+      document.querySelector('.movies').insertAdjacentHTML('beforeend', `
+        <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
+          <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+        </div>
+      `)
+
+      document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
+        event.preventDefault()
+        let movieElement = document.querySelector(`.movie-${movie.id}`)
+        if (movieElement.classList.contains(`opacity-20`)) {
+          movieElement.classList.remove('opacity-20')
+          await db.collection('watched').doc(`${movie.id}`).delete()
+        } else {
+          movieElement.classList.add('opacity-20')
+          await db.collection('watched').doc(`${movie.id}`).set({})
+        }
+      }) 
+    }
+  } else {
+    console.log('signed out')
+
+    document.querySelector('.welcome').classList.add('hidden')
+    // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    // FirebaseUI configuration
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'movies.html'
+    }
+    document.querySelector('.movies').classList.add('pt-14')
+    // Starts FirebaseUI Auth
+    ui.start('.movies', authUIConfig)
+
   }
 })
 
